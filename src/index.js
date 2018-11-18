491e9f20e0bfaf0a16a3fff32135e24e3acb57db
maurizioterreni <maurizio.terreni@live.it>
2018-11-18 15:02:30 +0100

    creata la griglia

--- src/index.js
+++ src/index.js
@@ -4,8 +4,32 @@ import './index.css';
 
 class Grid extends React.Component{
     render(){
+        const width = this.props.cols * 14;
+        var rowsArr = [];
+
+        var boxClass = "";
+        for(var i = 0; i < this.props.rows; i++){
+            for(var k = 0; k < this.props.cols; k++){
+                let boxId = i + "_" + k;
+                boxClass = this.props.gridFull[i][k] ? "box on" : "box off";
+
+                rowsArr.push(
+                    <Box
+                        boxClass = {boxClass}
+                        key = {boxId}
+                        boxId = {boxId}
+                        row = {i}
+                        col = {k}
+                        selectBox = {this.props.selectBox}
+                    />
+                );
+            }
+        }
+
         return (
-            <div>GRID</div>
+            <div className="grid" style={{width: width}}>
+            {{rowsArr}}
+            </div>
         );
     }
 }
@@ -13,15 +37,25 @@ class Grid extends React.Component{
 class Main extends React.Component{
     constructor (){
         super();
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+
         this.state = {
             generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
         }
     }
     render(){
         return(
             <div>
                 <h1>Game Of Life</h1>
-                <Grid></Grid>
+                <Grid 
+                    gridFull = {this.state.gridFull}
+                    rows = {this.rows}
+                    cols = {this.cols}
+                    selectBox = {this.selectBox}
+                    />
                 <h2>Generations: {this.state.generation}</h2>
             </div>
         );
